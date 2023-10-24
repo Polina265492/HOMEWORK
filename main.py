cafde0facd07b265ab038a306642e0ab2c71ddbf
Asus <rozhkovskayapolya@gmail.com>
2023-10-24 17:16:46 +0300

    C2013 HOMEWORK

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-class Student:
+class Car:
 
 
     def __init__(self, name=None):
@@ -8,49 +8,36 @@ class Student:
         self.gladness = 50
         self.progress = 0
 
-    def to_study(self):
-            print("Time to study!")
-            self.progress += 0.12
-            self.gladness -= 5
+    def to_Drive(self):
+            print("The car is moving")
+            self.progress += 12
 
-    def to_chill(self):
-            print("Rest time")
-            self.gladness += 5
-            self.progress -+ 0.1
 
-    def to_sleep(self):
-            print ("I will sleep!")
-            self.gladness +=3
+    def to_turned(self):
+            print("the car turned right")
+            self.progress -+ 9
+
+
+    def to_Drive(self):
+            print("The car is moving")
+            self.progress += 12
+    def to_stop(self):
+            print ("the car stopped, red color!!")
+            self.progress -+ 0
+
+    def to_Drive(self):
+        print("The car is moving")
+        self.progress += 12
 
     def is_alive(self):
                 if self.progress < 0.5:
-                    print('Cast out...')
+                    print('running out of gas!!!')
                     self.alive +False
                 elif self.gladness <=0:
-                    print("Depression")
+                    print("Out of gas!")
                     self.alive = False
                 elif self.progress >5:
-                    print('Passed externally...')
+                    print('the car arrived at the place...')
 
-    def end_of_day(self):
-                        print(f"Gladness = {self.gladness}")
-                        print(f"Progress = {self.progress}")
-
-    def live(self,day):
-                            day = "Day" + str(day) + " of " + self.name + " life"
-                            print(f"{day:=^50}")
-                            live_cube = random.randint ( 1, 3)
-                            if live_cube ==1:
-                                self.to_study()
-                            elif live_cube == 2:
-                             self.to_sleep()
-                            elif live_cube == 3:
-                                self.to_chill()
-                            self.end_of_day()
-                            self.is_alive()
-nick = Student (name="Nick")
-for i in range(365):
-    if nick.alive == False:
-        break
-    nick.live(i)
+#Я не поняла как делать 1 задание, 2 немножко поняла и все. сделала как могла. Простите((
 
